41133bdbb6037fc1ce2ee6e385ac36e77b8b3888
Ishaan Gaba <ishaangaba97@gmail.com>
2022-06-12 14:13:03 +0530

    Time: 40 ms (13.59%), Space: 19.4 MB (92.35%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate = 0;
+        int count = 0;
+        for(int num :nums){
+            if(count == 0){
+                candidate = num;
+            }
+            if(num == candidate) count+=1;
+            else count-=1;
+        }
+        return candidate;
+    }
+};

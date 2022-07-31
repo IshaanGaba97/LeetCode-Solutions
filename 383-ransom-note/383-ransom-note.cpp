cf96fcab399af3cd3feea635f0d173117138acc3
Ishaan Gaba <ishaangaba97@gmail.com>
2022-07-31 14:05:44 +0530

    Time: 23 ms (59.60%), Space: 8.8 MB (40.13%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> mp;
+        for(int i = 0; i < magazine.size(); i++){
+            mp[magazine[i]]++;
+        }
+        for(int i = 0; i < ransomNote.size(); i++){
+            if(mp[ransomNote[i]] == 0) return false;
+            else mp[ransomNote[i]]--;
+        }
+        return true;
+    }
+};

e859bc9283458b4497b0d44abca3c3339993f26f
Ishaan Gaba <ishaangaba97@gmail.com>
2023-04-29 14:24:46 +0530

    Time: 8 ms (50.13%), Space: 6.9 MB (73.65%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -10,8 +10,8 @@ public:
             mp2[t[i]] = s[i];
         }
         for(int i = 0; i < s.size(); i++){
-            if (mp1[s[i]] && mp1[s[i]]!=t[i]) return false;
-            if (mp2[t[i]] && mp2[t[i]]!=s[i]) return false;
+            if (mp1[s[i]]!=t[i]) return false;
+            if (mp2[t[i]]!=s[i]) return false;
         }  
         return true;
     }

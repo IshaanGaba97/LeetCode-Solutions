e3741060b5a97e98def289048a2ea7c0ad011f67
Ishaan Gaba <ishaangaba97@gmail.com>
2023-05-11 20:29:22 +0530

    Time: 21 ms (60.61%), Space: 13 MB (63.96%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -15,7 +15,22 @@ public:
         int m = nums2.size();
         vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
         
-        // for(int i = 0; i < )
-        return solve(0, 0, nums1, nums2, dp);
+        for(int i = 0; i <= n; i++){
+            dp[i][m] = 0;
+        }
+        for(int i = 0; i <= m; i++){
+            dp[n][i] = 0;
+        }
+        
+        for(int i = n-1; i >= 0; i--){
+            for(int j = m-1; j >= 0; j--){
+                if(nums1[i] == nums2[j]){
+                    dp[i][j] = 1 + dp[i+1][j+1];
+                } else {
+                     dp[i][j] = max(dp[i+1][j], dp[i][j+1]);
+                }
+            }
+        }
+        return dp[0][0];
     }
 };

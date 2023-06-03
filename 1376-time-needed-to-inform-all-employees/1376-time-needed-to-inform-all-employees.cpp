439514ca9661ed6bf66d6bd31c4fdec9399abed2
Ishaan Gaba <ishaangaba97@gmail.com>
2023-06-03 23:39:59 +0530

    Time: 494 ms (23.65%), Space: 157.7 MB (20.96%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
-    int dfs(int node, unordered_map<int, vector<int>> &adj, vector<int> &itime){
-        int headID = itime[node];
+    // total time is parent + max(child nodes);
+    int dfs(int rootnode, unordered_map<int, vector<int>> &adj, vector<int> &itime){
+        int rootTime = itime[rootnode];
         int mx = 0;
-        for(auto it : adj[node]){
+        
+        for(auto it : adj[rootnode]){
             mx = max(mx, dfs(it, adj, itime));
         }
-        return headID + mx;
+        
+        return rootTime + mx;
     }
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
         unordered_map<int, vector<int>> adj;
+        
+        //creating a n-ary tree
         for(int i = 0; i < manager.size(); i++){
             if(i == headID) continue;
             adj[manager[i]].push_back(i);
         }
+        
+        // calling dfs to get max out of all the childs
         int ans = dfs(headID, adj, informTime);
         return ans;
     }

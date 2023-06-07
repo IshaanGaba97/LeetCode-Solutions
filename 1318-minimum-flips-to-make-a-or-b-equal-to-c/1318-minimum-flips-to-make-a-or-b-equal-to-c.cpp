d434277a8e1d7eee083ba45cd5f350db678ba1a9
Ishaan Gaba <ishaangaba97@gmail.com>
2023-06-08 00:19:54 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (93.33%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -2,26 +2,10 @@ class Solution {
 public:
     int minFlips(int a, int b, int c) {
         int flip = 0;
-        while(a !=0 || b != 0 || c!=0){
-            int ar = a & 1;
-            int br = b & 1;
-            int cr = c & 1;
-            if(cr == 1){
-                if(ar == 0 && br == 0){
-                    flip++;
-                }
-                a >>= 1;
-                b >>= 1;
-            } else if(cr == 0){
-                if(ar == 1)
-                    flip++;
-                if(br == 1)
-                    flip++;
-                a >>= 1;
-                b >>= 1;
-            }
-            c >>= 1;
-        }
+        int ans = a | b;
+        int temp = ans ^ c;
+        flip += __builtin_popcount(temp);
+        flip += __builtin_popcount((a&b)&temp);
         return flip;
     }
 };

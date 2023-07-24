ea46a7281e4a1e02539a0f3bbfb5feb3b73a5ca9
Ishaan Gaba <ishaangaba97@gmail.com>
2023-07-24 17:17:02 +0530

    Time: 4 ms (40.18%), Space: 5.9 MB (92.36%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1.0;
+        long long nn = n;     //edge case prevent
+        if(nn < 0) nn = nn * -1;
+        while(nn != 0){
+            if(nn % 2){
+                ans = ans * x;
+                nn = nn - 1;
+            } else {
+                x = x * x;
+                nn = nn / 2;
+            }
+        }
+        if(n < 0) ans = (double)(1.0) / (double)(ans);
+        return ans;
+    }
+};
